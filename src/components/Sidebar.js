be78e3329710d24ff9df269d2a2adff39f07da3c
Gasim Gasimzada <gasim@appristas.io>
2019-01-28 02:42:13 +0400

    Add logo to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,52 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { StaticQuery, graphql, Link } from 'gatsby';
 
 import styles from './sidebar.module.css';
 
+import logo from '../assets/img/logo.svg';
+import { strictEqual } from 'assert';
+
 export default () => (
-  <aside className={styles.sidebar}>
-    <nav className={styles.nav}>
-      <ul>
-        <li>
-          <Link activeClassName={styles.active} to="/">
-            Yazılar
-          </Link>
-        </li>
-        <li>
-          <Link activeClassName={styles.active} to="/videos">
-            Videolar
-          </Link>
-        </li>
-        <li>
-          <Link activeClassName={styles.active} to="/courses">
-            Kurslar
-          </Link>
-        </li>
-        <li>
-          <Link activeClassName={styles.active} to="/events">
-            Tədbirlər
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </aside>
+  <StaticQuery
+    query={graphql`
+      query SidebarQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <aside className={styles.sidebar}>
+        <div className={styles.logo}>
+          <img src={logo} alt={data.site.siteMetadata.title} />
+        </div>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link activeClassName={styles.active} to="/">
+                Yazılar
+              </Link>
+            </li>
+            <li>
+              <Link activeClassName={styles.active} to="/videos">
+                Videolar
+              </Link>
+            </li>
+            <li>
+              <Link activeClassName={styles.active} to="/courses">
+                Kurslar
+              </Link>
+            </li>
+            <li>
+              <Link activeClassName={styles.active} to="/events">
+                Tədbirlər
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    )}
+  />
 );
